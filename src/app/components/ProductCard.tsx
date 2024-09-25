5fe5a10b418012108c71d877a568b0a0234d5d38
Alpha.liu <dittoliu@gmail.com>
2024-09-25 22:43:18 +0800

    feature: dynamic routes

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+import style from "./Product.module.css";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
