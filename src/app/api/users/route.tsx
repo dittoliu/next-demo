abc2ce743ce52c8eb1cd8f4b9cd151f007837302
Alpha.liu <dittoliu@gmail.com>
2024-10-01 00:36:29 +0800

    feature: add zod lib

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 export function GET(request: NextRequest) {
     return NextResponse.json([
@@ -15,8 +16,9 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
-    if(!body.name) {
-        return NextResponse.json({ error: 'Name is required' }, { status: 400 }  )
+    const validation = await schema.safeParse(body)
+    if(!validation.success) {
+        return NextResponse.json(validation.error.errors, { status: 400 }  )
     }
     return NextResponse.json({
         id: body.id,
