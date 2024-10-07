3460a501d70e7fd7424b0100ed7239d77cac1e94
Alpha <dittoliu@gmail.com>
2024-10-07 20:23:49 +0800

    feature: add upload page

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, { useState } from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState('')
+
+  return (
+    <>
+      {
+        publicId && (
+          <CldImage src={publicId} width={300} height={180} alt='' />
+        )
+      }
+      <CldUploadWidget
+        uploadPreset="w1hdk94b"
+        onSuccess={(result, widget) => {
+          if (result.event !== 'success') return
+          const info = result.info as CloudinaryResult
+          setPublicId(info.public_id)
+        }}
+      >
+        {({ open }) => {
+          return (
+            <button className='btn btn-primary'
+              onClick={() => open()}
+            >
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
+  )
+}
+
+export default UploadPage
