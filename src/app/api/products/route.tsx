04b797612a234646249c72dceba41aa78ed21bc1
Alpha <dittoliu@gmail.com>
2024-10-07 17:15:23 +0800

    feature: add product model and update product api

--- src/app/api/products/route.tsx
+++ src/app/api/products/route.tsx
@@ -1,30 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
 import schema from "./schema";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    {
-      id: 1,
-      name: 'milk',
-      price: 2.5
-    },
-    {
-      id: 2,
-      name: 'bread',
-      price: 3.5
-    },
-  ])
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany()
+  return NextResponse.json(products)
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
   const validation = await schema.safeParse(body)
-  if(!validation.success) {
-      return NextResponse.json(validation.error.errors, { status: 400 }  )
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 })
   }
-  return NextResponse.json({
-      id: 10,
+
+  const product = await prisma.product.findUnique({
+    where: { name: body.name },
+  })
+
+  if (product) {
+    return NextResponse.json({ error: 'Product with this name already exists' }, { status: 400 })
+  }
+
+  const createdProduct = await prisma.product.create({
+    data: {
       name: body.name,
-      price: body.price
-  }, { status: 201})
+      price: body.price,
+    }
+  })
+  return NextResponse.json(createdProduct, { status: 201 })
 }
