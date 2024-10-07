14fd2a7f7ed1b26153100c9048846c104f767f21
Alpha <dittoliu@gmail.com>
2024-10-07 16:37:37 +0800

    feature: add CRUD apis of users

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -10,11 +10,23 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
     const body = await request.json()
     const validation = await schema.safeParse(body)
-    if(!validation.success) {
-        return NextResponse.json(validation.error.errors, { status: 400 }  )
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors, { status: 400 })
     }
-    return NextResponse.json({
-        id: body.id,
-        name: body.name
-    }, { status: 201})
+
+    const user = await prisma.user.findUnique({
+        where: { email: body.email },
+    })
+
+    if (user) {
+        return NextResponse.json({ error: 'User with this email already exists' }, { status: 400 })
+    }
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email,
+        }
+    })
+    return NextResponse.json(newUser, { status: 201 })
 }
