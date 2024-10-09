9cf12856082744f349e93a6fe9af640510a6c6af
Alpha <dittoliu@gmail.com>
2024-10-09 09:24:15 +0800

    feature: get session on server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
   return (
     <main>
-      This is Home Page
+      <h1>Welcome, {session && session.user!.name}!</h1>
       <ProductCard />
     </main>
   );
